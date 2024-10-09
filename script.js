17073e91c2e82074a426785a8712acc3e8368b97
to0mas <rehanektomas123@gmail.com>
2024-10-09 16:20:50 +0200

    .

--- script.js
+++ script.js
@@ -6,6 +6,43 @@ hamburger.onclick = function(){
 
 
 
+gsap.to('.horizontal-content', {
+    xPercent: -100 * (document.querySelectorAll('.box').length - 1),
+    ease: 'none',
+    scrollTrigger: {
+        trigger: '.horizontal-scroll',
+        start: 'top top',
+        end: '+=2000',
+        pin: true,
+        scrub: true,
+    }
+});
+
+// 4. Draggovatelný box
+Draggable.create(".draggable-box", {
+    type: "x,y",
+    edgeResistance: 0.65,
+    bounds: ".horizontal-scroll",
+    inertia: true
+});
+
+// 5. Návrat k vertikálnímu scrollování po 300vh
+gsap.fromTo('.resume-text',
+    { opacity: 0, y: 50 },
+    {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: '.vertical-resume',
+            start: 'top center',
+            end: 'top 100px',
+            scrub: true,
+        }
+    }
+);
+
+
 
 
 
